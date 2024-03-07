0d77c7e6f44d8e8e56845820ffdaf896ada4ab81
Hazmats2145 <146298053+2145-Hazmats@users.noreply.github.com>
2024-03-06 20:51:54 -0500

    Spark Max Changes
    Test this code to make sure it works before merging this branch into main.

--- src/main/java/frc/robot/subsystems/BoxSubsystem.java
+++ src/main/java/frc/robot/subsystems/BoxSubsystem.java
@@ -15,48 +15,60 @@ import com.revrobotics.SparkLimitSwitch.Type;
 import frc.robot.Constants;
 import frc.robot.Constants.BoxConstants;
 import frc.robot.Constants.ArmConstants.ArmState;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 
 public class BoxSubsystem extends SubsystemBase {
   // Declare and intialize motor variables to a new instance of CANSparkMax
   private CANSparkMax shooterMotor = new CANSparkMax(BoxConstants.kShooterMotorID, MotorType.kBrushless);
   private CANSparkMax theOtherShooterMotor = new CANSparkMax(BoxConstants.kTheOtherShooterMotorID, MotorType.kBrushless);
-
   private CANSparkMax intakeMotor = new CANSparkMax(BoxConstants.kIntakeMotorID, MotorType.kBrushless);
   private final RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
   // Speed of the shooter motor
   private double shooterMotorSpeed = 0.0;
   private double shooterChargeTime = Constants.BoxConstants.kShooterDelay;
 
-
   /** Creates a new Box. */
   public BoxSubsystem() {
-    // Enable voltage compensation
-    shooterMotor.restoreFactoryDefaults();
-    theOtherShooterMotor.restoreFactoryDefaults();   
+    /* Motor Configuration */
 
+    // Restore factory defaults of the Spark Max.
+    // It's important to have all Spark Maxs behave the expected way, especially if we switch to a different Spark Max in the middle of a competition.
+    shooterMotor.restoreFactoryDefaults();
+    theOtherShooterMotor.restoreFactoryDefaults();
+    intakeMotor.restoreFactoryDefaults();
+
+    // Set motor current limit
     shooterMotor.setSmartCurrentLimit(40);
     theOtherShooterMotor.setSmartCurrentLimit(40);
+    intakeMotor.setSmartCurrentLimit(40);
 
-
+    // Enable voltage compensation
     intakeMotor.enableVoltageCompensation(BoxConstants.kIntakeMotorNominalVoltage);
     shooterMotor.enableVoltageCompensation(BoxConstants.kShooterMotorNominalVoltage);
     theOtherShooterMotor.enableVoltageCompensation(BoxConstants.kShooterMotorNominalVoltage);
-    //set the other shooter motor to follow the OG
-    theOtherShooterMotor.follow(shooterMotor);
-    // Invert the shooter motor
-    intakeMotor.setInverted(false);
-    shooterMotor.setInverted(true);
-    theOtherShooterMotor.setInverted(true);
+
+    // Set the idle mode of the motors
     shooterMotor.setIdleMode(IdleMode.kCoast);
     theOtherShooterMotor.setIdleMode(IdleMode.kCoast);
+    // HELP! Should the intake motor be in coast or brake mode?
+    //intakeMotor.setIdleMode(IdleMode.kCoast);
+
+    // Invert the shooter motor
+    shooterMotor.setInverted(true);
+
+    // Have the second shooter motor follow the first
+    theOtherShooterMotor.follow(shooterMotor);
+
+    /* Encoder Configuration */
+
+    /* PIDControllers */
+
   }
+  
 
   /**
    * Sets the speed of the intake motor. Takes a double for speed and a boolean for reversed.
@@ -68,13 +80,16 @@ public class BoxSubsystem extends SubsystemBase {
     return run(() -> intakeMotor.set(speed));
   }
 
+
   public Command setIntakeMotorCommandAuto(double speed){
     return runOnce(() -> intakeMotor.set(speed));
   }
 
+
   public Command setIntakeMotorCommandThenStop(double speed) {
     return Commands.startEnd(() -> intakeMotor.set(speed), () -> intakeMotor.set(0), this);
   }
+
 
   public Command ShootNoteAmp() {
     return setShooterMotorCommand(ArmSubsystem::getArmState)
@@ -82,6 +97,7 @@ public class BoxSubsystem extends SubsystemBase {
     .andThen(setIntakeMotorCommand(BoxConstants.kFeedSpeed))
     .withTimeout(2.0);
   }
+
 
   public Command ShootNoteSubwoofer() {
     return setIntakeMotorCommandThenStop(Constants.BoxConstants.kRegurgitateSpeed)
@@ -92,7 +108,9 @@ public class BoxSubsystem extends SubsystemBase {
     .withTimeout(2.0)
     .andThen(setShooterMotorCommand(0));
   }
-public Command ShootNoteSubwooferNoRegurgitate() {
+
+
+  public Command ShootNoteSubwooferNoRegurgitate() {
     return //setIntakeMotorCommandThenStop(Constants.BoxConstants.kRegurgitateSpeed)
     //.withTimeout(.25) 
     setShooterMotorCommand(0.34)
@@ -102,12 +120,14 @@ public Command ShootNoteSubwooferNoRegurgitate() {
     .andThen(setShooterMotorCommand(0));
   }
 
+
   public Command ShootNoteAuton() {
     return setShooterMotorCommand(ArmSubsystem::getArmState)
     .withTimeout(getChargeTime(ArmSubsystem::getArmState))
     .andThen(setIntakeMotorCommand(BoxConstants.kFeedSpeed))
     .withTimeout(2.0);
   }
+
 
   /**
    * Sets the speed of the shooter motor.
@@ -118,17 +138,19 @@ public Command ShootNoteSubwooferNoRegurgitate() {
     return run(() -> shooterMotor.set(speed));
   }
 
- public Command setTheOtherShooterMotorCommand(double speed) {
+
+  public Command setTheOtherShooterMotorCommand(double speed) {
     return run(() -> theOtherShooterMotor.set(speed));
   }
+
 
   public Command setShooterMotorCommandThenStop(double speed) {
     return Commands.startEnd(() -> shooterMotor.set(speed),() -> shooterMotor.set(0),this);
   }
 
+
   public Command setShooterMotorCommand(Supplier<ArmState> position) {
     return run(() -> {
-
       SmartDashboard.putString("Shooter Motor Command Position", position.get().toString());
 
       switch(position.get()) {
@@ -148,10 +170,10 @@ public Command ShootNoteSubwooferNoRegurgitate() {
           shooterMotorSpeed = BoxConstants.kDefaultShootSpeed;
           break;
       }
-
       shooterMotor.set(shooterMotorSpeed);
     });
   }
+
 
   // This is broken it always returns the intialized shooterChargeTime value
   public double getChargeTime(Supplier<ArmState> position) {
@@ -164,10 +186,9 @@ public Command ShootNoteSubwooferNoRegurgitate() {
           shooterMotorSpeed = BoxConstants.kShooterDelay;
           break;
       }
-
     return shooterChargeTime;
-
   }
+
 
   /**
    * Stops the intake and shooter motor.
@@ -185,12 +206,14 @@ public Command ShootNoteSubwooferNoRegurgitate() {
     return intakeEncoder.getVelocity();
   }
 
+
   /**
    * Returns a boolean based on if the forward limit switch is pressed.
    */
   public boolean isReverseLimitSwitchPressed() {
     return intakeMotor.getReverseLimitSwitch(Type.kNormallyOpen).isPressed();
   }
+
 
   @Override
   public void periodic() {
